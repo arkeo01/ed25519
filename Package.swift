9816dbf3319c1454418b8767cbe7b72453711c3f
arkeo <43909991+arkeo01@users.noreply.github.com>
2018-11-23 18:58:12 +0530

    Change in PackageDescription
    PackageDescription API has been changed from v3 to v4.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "Ed25519",
     targets: [
-        Target(name: "Ed25519", dependencies: ["CEd25519"])
+        .target(
+            name: "Ed25519", 
+            dependencies: ["CEd25519"]
+        )
     ]
 )
